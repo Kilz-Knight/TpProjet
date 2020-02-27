3c31ce67787fef9a9286761c6d843019dada3989
Kilz Knight <kilzknightytb@hotmail.com>
2020-02-26 19:01:56 -0500

    Ajout de la barre de défilement

--- js/fonctions.js
+++ js/fonctions.js
@@ -5,9 +5,16 @@ var btn = document.querySelector("#button");;
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 40) {
     btn.classList.add('show');
+    document.querySelector(".progress-container").style.display = "block";
   } else {
     btn.classList.remove('show');
+    document.querySelector(".progress-container").style.display = "none";
   }
+
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
 
 // When the user clicks on the button, scroll to the top of the document
